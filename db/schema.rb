53ba2f486beef51a16faca980a59e89dd6fb08d1
daisuke <daisuke.satou2@e-harp.jp>
2016-02-10 16:37:10 +0900

    rubocopで自動コード修正してみた

--- db/schema.rb
+++ db/schema.rb
@@ -11,33 +11,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160101034710) do
-
-  create_table "hvalues", force: :cascade do |t|
-    t.string   "cityid"
-    t.string   "N03_004"
-    t.string   "bureau"
-    t.integer  "ttl"
-    t.integer  "gw"
-    t.integer  "frs"
-    t.integer  "ib"
-    t.integer  "ft"
-    t.integer  "rs"
-    t.integer  "ss"
-    t.integer  "eac"
-    t.integer  "eps"
-    t.integer  "bu"
-    t.integer  "pa"
-    t.integer  "et"
-    t.integer  "mail"
-    t.integer  "vender"
-    t.integer  "ns"
-    t.integer  "cms"
-    t.integer  "ca"
-    t.integer  "fn"
-    t.integer  "ps"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema.define(version: 20_160_101_034_710) do
+  create_table 'hvalues', force: :cascade do |t|
+    t.string   'cityid'
+    t.string   'N03_004'
+    t.string   'bureau'
+    t.integer  'ttl'
+    t.integer  'gw'
+    t.integer  'frs'
+    t.integer  'ib'
+    t.integer  'ft'
+    t.integer  'rs'
+    t.integer  'ss'
+    t.integer  'eac'
+    t.integer  'eps'
+    t.integer  'bu'
+    t.integer  'pa'
+    t.integer  'et'
+    t.integer  'mail'
+    t.integer  'vender'
+    t.integer  'ns'
+    t.integer  'cms'
+    t.integer  'ca'
+    t.integer  'fn'
+    t.integer  'ps'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
