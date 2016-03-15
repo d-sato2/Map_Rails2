892b81395db5b982a04fb402751ff515981f7516
daisuke <daisuke.satou2@e-harp.jp>
2016-03-16 08:55:47 +0900

    モーダル内容変更、excelダウンロードでカラム名と内容を指定できるように修正

--- app/models/hvalue.rb
+++ app/models/hvalue.rb
@@ -9,17 +9,23 @@ class Hvalue < ActiveRecord::Base
     CSV.generate(options) do |csv|
       csv << csv_column_names
       all.each do |hvalue|
-        csv << hvalue.attributes.values_at(*column_names)
+        csv << hvalue.csv_column_values
       end
     end
   end
 
   def self.csv_column_names
-    %W(ID 自治体コード 自治体名 振興局 人口 GW 施設予約 ICT-BCP とどくん リソース 校務支援 電子申請 電子調達 バックアップ 公会計 eLTax Mail 契約ベンダ 任用支援 CMS 総合行政 ふるさと納税 人事給与 \u4F5C\u6210\u65E5\u6642 \u66F4\u65B0\u65E5\u6642 HARP協議会 HARP基盤 eLTax 公会計 校務 とどくん 備考欄２ 備考欄１)
+    %W(ID 自治体コード 自治体名 振興局 人口 GW 施設予約 ICT-BCP とどくん
+      リソース 校務支援 電子申請 電子調達 バックアップ 公会計 eLTax Mail 契約ベンダ
+      任用支援 CMS 総合行政 ふるさと納税 人事給与 作成日時 更新日時 HARP協議会
+      HARP基盤 eLTax 公会計 校務 とどくん 備考欄２ 備考欄１)
   end
 
   def csv_column_values
-    [id, cityid, cityname, ttl, gw, frs, ib, ft, rs, ss, eac, eps, bu, pa, et, mail, vender, ns, cms, ca, fn, ps]
+    [id, cityid, cityname, bureau, ttl, gw, frs, ib, ft,
+      rs, ss, eac, eps, bu, pa, et, mail, vender,
+      ns, cms, ca, fn, ps, created_at, updated_at, kyougi,
+      kiban, el_kyodo, kokaike, koumu, todoku, memo_kiban, memo_service]
   end
 
   def self.search(search)
